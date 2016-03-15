6aa866c6762bb3c56e2b1253b7e0f224b4ffa924
jadefrh <jadefrh@gmail.com>
2016-03-15 18:20:02 +0100

    Add navbar (first elements)

--- app/view/module/includes/navbar.php
+++ app/view/module/includes/navbar.php
@@ -1,2 +1,13 @@
-Bienvenue sur le site <?= $data['user']->get('firstname') ?><br>
-<br>
+<header class="navbar">
+	<div class="container">
+		<img src="/assets/img/logo.png" class="logo">
+
+		<div class="nav-right">
+			<a href="/auth/logout" class="logout">Déconnexion</a>
+			<div class="avatar"></div>
+			<div class="infos">
+				<?= $data['user']->get('firstname'); ?> <?= strtoupper($data['user']->get('lastname')); ?>
+				<a href="/auth/settings">éditer mon profil</a>
+		</div>
+	</div>
+</header>
