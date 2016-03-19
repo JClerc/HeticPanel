e76563d7a21d1e887f6e68e6df3c4276f66b7f1a
jadefrh <jadefrh@gmail.com>
2016-03-20 00:29:42 +0100

    Add design homepage panel

--- app/view/page/panel/index.php
+++ app/view/page/panel/index.php
@@ -3,8 +3,29 @@
 
 <?php $data['flash']->display() ?>
 
-Choisis une activité:<br>
-<a href="/panel/justify/">Réception des justificatifs</a><br>
-<a href="/panel/course/">Liste des présents</a><br>
-<a href="/panel/cancel/">Annuler un cours</a><br>
-<a href="/admin/">Administration</a><br>
+<h3 class="title">
+    Bonjour 
+    <span><?= substr($data['user']->get('firstname'), 0, 1); ?>. <?= strtoupper($data['user']->get('lastname')); ?></span>
+</h3>
+
+<section class="container">
+    <a href="/panel/justify" class="dashbox sided">
+        <h3 class="box-title">Réception des justificatifs</h3>
+    </a>
+
+    <a href="/panel/course" class="dashbox sided">
+        <h3 class="box-title">Liste des absents</h3>
+    </a>
+
+    <div class="clear"></div>
+</section>
+
+<section class="container" style="margin-top: 20px">
+    <a href="/panel/cancel" class="dashbox sided">
+        <h3 class="box-title">Annuler un cours</h3>
+    </a>
+
+    <a href="/admin/" class="dashbox sided">
+        <h3 class="box-title">Administration</h3>
+    </a>
+</section>
