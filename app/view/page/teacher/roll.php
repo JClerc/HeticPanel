5633514cdf0da5958166220624a361a15578e7b2
jadefrh <jadefrh@gmail.com>
2016-03-20 00:06:34 +0100

    Add new design appel

--- app/view/page/teacher/roll.php
+++ app/view/page/teacher/roll.php
@@ -5,60 +5,37 @@
 
 <?php $data['flash']->display() ?>
 
-<?php
-    $count = ceil(count($data['students']) / 3);
-?>
-
 <h3 class="title">Faire l'appel</h3>
 
 <section class="container">
     <div class="dashbox dashbox-students">
         <h3 class="course-title">Cours du <?= date('d/m/Y') ?></h3>
-        <h4 class="course-subtitle">Cochez uniquement les absences</h4>
 
         <!-- Formulaire a envoyer une fois l'appel terminé -->
         <!-- -> $(form).submit() -->
         <form method="post" class="students-form">
-            <!-- 
-                // data-index = index de l'éleve dans la liste
-                // A utiliser pour passer au suivant
-                // Le premier a la classe "current", et le deuxieme a la classe "next"
-                <div class="student current" data-index="0">
-                    <div class="name">
-                        // Le nom de l'élève
-                        Bastien BERGAGLIA
-                    </div>
-                    <div class="hidden">
-                        // Case a cocher ou pas en Javascript
-                        // "value" sera préremplis en PHP
-                        <input type="checkbox" value="4" name="absences[]">
-                    </div>
-                </div>
-             -->
-            <?php for ($offset = 1; $offset <= 3; $offset++): ?>
-                <div class="students">
-                    <?php 
-                    $last = ($offset == 3) ? $offset-1 : 1;
-                    for ($i = $count * ($offset-1); $i <= ($count * $offset) - $last; $i++): 
-                        $student = $data['students'][$i];
-                    ?>
-                        <div class="student <?= $student['class'] ?>" data-index="<?= $student['index'] ?>">
-                            <label for="<?= $student['user']->getId() ?>" class="name">
-                                <?= $student['user']->get('firstname') ?> <?= $student['user']->get('lastname') ?> 
-                            </label>
-                            <div class="hidden">
-                                <input type="checkbox" id="<?= $student['user']->getId() ?>" value="<?= $student['user']->getId() ?>" name="absences[]">
-                            </div>
+            <div class="roll-students" data-count="<?= count($data['students']) ?>">
+                <?php foreach ($data['students'] as $index => $student): ?>
+                    <div class="roll-student <?= $student['class'] ?>" data-index="<?= $index ?>">
+                        <label for="student-<?= $student['user']->getId() ?>" class="name">
+                            <?= $student['user']->get('firstname') ?> <?= $student['user']->get('lastname') ?> 
+                        </label>
+                        <div class="hidden">
+                            <input type="checkbox" id="student-<?= $student['user']->getId() ?>" value="<?= $student['user']->getId() ?>" name="absences[]">
                         </div>
-                    <?php endfor; ?>
-                </div>
-            <?php endfor; ?>
+                    </div>
+                <?php endforeach; ?>
+            </div>
         </form>
     </div>
-
-    <div class="students-submit-cont">
-        <button class="students-submit" onclick="$('.students-form').submit();">Envoyer</button>
-    </div>
-
-    <br><br>
 </section>
+
+<script>
+// CHARGE ROLL.JS APRES JQUERY
+(function () {
+    var s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.src = '/assets/js/app/roll.js';
+    document.body.appendChild(s);
+})();
+</script>
---
+++ assets/js/app/roll.js
@@ -0,0 +1,49 @@
+$.fn.scrollTo = function( target, options, callback ){
+    if(typeof options == 'function' && arguments.length == 2){ callback = options; options = target; }
+        var settings = $.extend({
+            scrollTarget  : target,
+            offsetTop     : 350,
+            duration      : 500,
+            easing        : 'swing'
+        }, options);
+    
+    return this.each(function(){
+        var scrollPane = $(this);
+        var scrollTarget = (typeof settings.scrollTarget == "number") ? settings.scrollTarget : $(settings.scrollTarget);
+        var scrollY = (typeof scrollTarget == "number") ? scrollTarget : scrollTarget.offset().top + scrollPane.scrollTop() - parseInt(settings.offsetTop);
+        scrollPane.animate({scrollTop : scrollY }, parseInt(settings.duration), settings.easing, function(){
+            if (typeof callback == 'function') { callback.call(this); }
+        });
+    });
+};
+
+function roll(next) {
+    var count = parseInt($('.roll-students').data('count'));
+
+    // Si fin de l'appel on valide le formulaire
+    if (next >= count) {
+        $('.students-form').submit();
+        return;
+    }
+
+    $('.roll-student.current').removeClass('current');
+    $('.roll-student[data-index="'+ next +'"]').addClass('current');
+    $('.roll-students').scrollTo('.roll-student[data-index="'+ next +'"]');
+}
+
+$(document).on('keyup', function (e) {
+    e.preventDefault();
+    var current = $('.roll-student.current');
+    var index = parseInt(current.data('index'));
+
+    switch (e.keyCode) {
+        case 13: // Absent
+            current.children('.hidden').children('input').attr('checked', true);
+            current.addClass('missing');
+            roll(index+1);
+            break;
+        case 32: // Present
+            roll(index+1);
+            break;
+    }
+});
