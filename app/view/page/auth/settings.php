326260c6e17254f045c729221db88770abd1d22b
jadefrh <jadefrh@gmail.com>
2016-03-20 00:41:34 +0100

    Add settings design

--- app/view/page/auth/settings.php
+++ app/view/page/auth/settings.php
@@ -2,14 +2,23 @@
 <?php $this->view('module/includes/navbar') ?>
 
 <div class="container">
-    <h3 class="title">Mes absences</h3>
+    <h3 class="title">Mon compte</h3>
 
     <?php $data['flash']->display() ?>
-
+    
     <form method="post">
-        <input type="password" name="password" placeholder="Mot de passe">
-        <input type="password" name="confirm" placeholder="Confirmez">
-        <button type="submit">Valider</button>
-    </form>
+        <div class="dashbox dashbox-settings">
+            <label>Nouveau mot de passe</label><br>
+            <input class="settings-input" type="password" name="password" placeholder="Mot de passe">
+            
+            <br><br>
 
+            <label>Confirmer le mot de passe</label><br>
+            <input class="settings-input" type="password" name="confirm" placeholder="Confirmez">
+        </div>
+
+        <div class="students-submit-cont">
+            <button class="students-submit" type="submit">Modifier le mot de passe</button>
+        </div>
+    </form>
 </div>
