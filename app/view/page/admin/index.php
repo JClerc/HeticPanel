198cbf0e35c01e649e4162f1a0fe197ef32a5c0f
JClerc <jices25@gmail.com>
2016-03-19 23:36:34 +0100

    Add admin page

---
+++ app/view/page/admin/index.php
@@ -0,0 +1,10 @@
+
+<?php $this->view('module/includes/navbar') ?>
+
+<?php $data['flash']->display() ?>
+
+Modifier:<br>
+<a href="/admin/promotion/">Gérer les promotions</a><br>
+<a href="/admin/group/">Gérer les groupes</a><br>
+<a href="/admin/course/">Gérer les cours</a><br>
+<a href="/admin/user/">Gérer les utilisateurs</a><br>
