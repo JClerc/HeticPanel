730abd43f8e62019f43e7e48b854ce9a3648b260
jadefrh <jadefrh@gmail.com>
2016-03-20 16:03:05 +0100

    Add design "gestion groupe"

--- app/view/page/admin/group.php
+++ app/view/page/admin/group.php
@@ -1,106 +1,143 @@
 
 <?php $this->view('module/includes/navbar') ?>
 
-<?php $data['flash']->display() ?>
+<section class="container">
+    <?php $data['flash']->display() ?>
 
-
-<!-- Ajouter -->
-<div class="menu menu-add">
-    <form method="post">
+    <h3 class="title">Gérer les cours</h3>
     
-        <!-- Quand on clic la dessus, ca affiche le formulaire -->
-        <a href="#" class="menu-title" onclick="$(this).parent().find('.menu-hidden').toggle();">
-            Ajouter un groupe
-        </a>
-
-        <div class="menu-hidden" style="<?= (isset($data['method-add']) and $data['method-add']) ? '' : 'display: none;' ?>">
-
-            <input type="number" name="index" class="menu-input" placeholder="Index du groupe (Ex: Pour G1 entrez 1)" value="1">
-
-            <select name="promotion" class="menu-select">
-                <?php foreach ($data['promotions'] as $promotion): ?>
-                    <option value="<?= $promotion->getId() ?>">P<?= $promotion->getYear() ?></option>
-                <?php endforeach; ?>
-            </select>
-
-            <button type="submit" class="btn-submit" name="method" value="add">Valider</button>
-        </div>
-
-    </form>
-</div>
+    <div class="course-list">
+        <!-- Ajouter -->
+        <div class="dashbox nopadding menu menu-add">
+            <!-- Quand on clic la dessus, ca affiche le formulaire -->
+            <a href="#" class="item missing menu-title" onclick="$('#add').toggle();">
+                Ajouter un groupe
+            </a>
+        </div><br>
 
 
-<!-- Modifier -->
-<div class="menu menu-edit">
-    <form method="post">
-        
-        <div class="menu-title">
-            Modifier un groupe
-        </div>
+        <!-- Modifier -->
+        <div class="dashbox nopadding menu menu-edit">
+            <form method="post">
+                <div class="item parent menu-title">
+                    Modifier un groupe
+                </div>
 
-        <!-- On choisis celui a modifier -->
-        <div class="menu-select">
-            <select name="edit">
-                <option value="0">Sélectionnez un groupe</option>
-                <?php foreach ($data['groups'] as $title => $groups): ?>
-                    <optgroup label="<?= $title ?>"></optgroup>
-                    <?php foreach ($groups as $group): ?>
-                        <option value="<?= $group->getId() ?>" <?= (isset($data['values']) and $data['values']['group']->getId() === $group->getId()) ? 'selected' : '' ?>>G<?= $group->getIndex() ?></option>
-                    <?php endforeach; ?>
-                <?php endforeach; ?>
-            </select>
-        </div>
+                <!-- On choisis celui a modifier -->
+                <div class="item menu-select">
+                    <select name="edit" style="width: 100%">
+                        <option value="0">Sélectionnez un groupe</option>
+                        <?php foreach ($data['groups'] as $title => $groups): ?>
+                            <optgroup label="<?= $title ?>"></optgroup>
+                            <?php foreach ($groups as $group): ?>
+                                <option value="<?= $group->getId() ?>" <?= (isset($data['values']) and $data['values']['group']->getId() === $group->getId()) ? 'selected' : '' ?>>G<?= $group->getIndex() ?></option>
+                            <?php endforeach; ?>
+                        <?php endforeach; ?>
+                    </select>
+                </div>
 
-        <!-- 1. On clique sur modifier -->
-        <button type="submit" class="btn-submit" name="method" value="retrieve">Modifier</button>
+                <div class="item" style="margin-top: -20px; height: 80px">
+                    <center>
+                        <button type="submit" class="absence-submit btn-submit" style="margin-top: 0" name="method" value="retrieve">Modifier</button>
+                    </center>
+                </div>
+            </form>
+        </div><br>
+
+        <div class="dashbox nopadding menu menu-delete">
+            <form method="post">
+                <div class="item parent menu-title">
+                    Supprimer un groupe
+                </div>
+
+                <!-- On choisis celui a supprimer -->
+                <div class="item menu-select">
+                    <select name="delete">
+                        <option value="0">Sélectionnez un groupe</option>
+                        <?php foreach ($data['groups'] as $title => $groups): ?>
+                            <optgroup label="<?= $title ?>"></optgroup>
+                            <?php foreach ($groups as $group): ?>
+                                <option value="<?= $group->getId() ?>">G<?= $group->getIndex() ?></option>
+                            <?php endforeach; ?>
+                        <?php endforeach; ?>
+                    </select>
+                </div>
+                
+                <div class="item" style="margin-top: -20px; height: 80px">
+                    <center>
+                        <button type="submit" class="absence-submit btn-submit" style="margin-top: 0" name="method" value="delete">Valider</button>
+                    </center>
+                </div>
+            </form>
+        </div><br>
+    </div>
+
+    <div class="calendar dashbox">
+        <form method="post" id="add" class="menu-hidden" style="<?= (isset($data['method-add']) and $data['method-add']) ? '' : 'display: none;' ?>">
+            <h3>Ajouter un groupe</h3>
+
+            <div class="form-container">
+                <div class="form-item">
+                    <label>Index du groupe</label><br>
+                    <input type="number" name="index" class="menu-input" placeholder="Index du groupe (Ex: Pour G1 entrez 1)" value="1">
+                    <br><br>
+                    
+                    <label>Promotion</label><br>
+                    <select name="promotion" class="menu-select">
+                        <?php foreach ($data['promotions'] as $promotion): ?>
+                            <option value="<?= $promotion->getId() ?>">P<?= $promotion->getYear() ?></option>
+                        <?php endforeach; ?>
+                    </select>
+                    <br><br>
+                </div>
+                <div class="form-item">&nbsp;</div>
+                <div class="form-item">&nbsp;</div>
+            </div>
+
+            <center>
+                <button type="submit" class="absence-submit btn-submit" name="method" value="add">Valider</button>
+            </center>
+        </form>
 
         <?php if (isset($data['values'])): ?>
+            <form method="post" class="menu-hidden">
+                <h3>Modifier le groupe</h3>
 
-            <!-- 2. Ca charge les données dans le formulaire -->
-            <div class="menu-hidden">
-
-                <input type="number" name="index" class="menu-input" placeholder="Index du groupe (Ex: Pour G1 entrez 1)" value="<?= $data['values']['group']->getIndex() ?>">
-        
-                <select name="promotion" class="menu-select">
-                    <?php foreach ($data['promotions'] as $promotion): ?>
-                        <option value="<?= $promotion->getId() ?>" <?= $data['values']['promotion'] === $promotion->getId() ? 'selected' : '' ?>>P<?= $promotion->getYear() ?></option>
+                <select name="edit" style="display: none;">
+                    <option value="0">Sélectionnez un groupe</option>
+                    <?php foreach ($data['groups'] as $title => $groups): ?>
+                        <optgroup label="<?= $title ?>"></optgroup>
+                        <?php foreach ($groups as $group): ?>
+                            <option value="<?= $group->getId() ?>" <?= (isset($data['values']) and $data['values']['group']->getId() === $group->getId()) ? 'selected' : '' ?>>G<?= $group->getIndex() ?></option>
+                        <?php endforeach; ?>
                     <?php endforeach; ?>
                 </select>
 
+                <div class="form-container">
+                    <div class="form-item">
+                        <label>Index du groupe</label><br>
+                        <input type="number" name="index" class="menu-input" placeholder="Index du groupe (Ex: Pour G1 entrez 1)" value="<?= $data['values']['group']->getIndex() ?>">
+                        <br><br>
+
+                        <label>Promotion</label><br>
+                        <select name="promotion" class="menu-select">
+                            <?php foreach ($data['promotions'] as $promotion): ?>
+                                <option value="<?= $promotion->getId() ?>" <?= $data['values']['promotion'] === $promotion->getId() ? 'selected' : '' ?>>P<?= $promotion->getYear() ?></option>
+                            <?php endforeach; ?>
+                        </select>
+                        <br><br>
+                    </div>
+                    <div class="form-item">&nbsp;</div>
+                    <div class="form-item">&nbsp;</div>
+                </div>
+
                 <input type="hidden" name="update" value="<?= $data['values']['group']->getId() ?>">
-
-                <button type="submit" class="btn-submit" name="method" value="update">Valider</button>
-            </div>
-
+                
+                <center>
+                    <button type="submit" class="absence-submit btn-submit" name="method" value="update">Valider</button>
+                </center>
+            </form>
         <?php endif; ?>
-
-    </form>
-</div>
-
-
-
-<div class="menu menu-delete">
-    <form method="post">
-    
-        <div class="menu-title">
-            Supprimer un groupe
-        </div>
-
-        <!-- On choisis celui a supprimer -->
-        <div class="menu-select">
-            <select name="delete">
-                <option value="0">Sélectionnez un groupe</option>
-                <?php foreach ($data['groups'] as $title => $groups): ?>
-                    <optgroup label="<?= $title ?>"></optgroup>
-                    <?php foreach ($groups as $group): ?>
-                        <option value="<?= $group->getId() ?>">G<?= $group->getIndex() ?></option>
-                    <?php endforeach; ?>
-                <?php endforeach; ?>
-            </select>
-        </div>
-
-        <button type="submit" class="btn-submit" name="method" value="delete">Valider</button>
-
-    </form>
-</div>
+    </div>
+</section>
 
