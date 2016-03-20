de05e16a6b6cd256c8704856cb269232f709946a
jadefrh <jadefrh@gmail.com>
2016-03-20 16:25:35 +0100

    Add design "gestion promotion"

--- app/view/page/admin/promotion.php
+++ app/view/page/admin/promotion.php
@@ -1,88 +1,100 @@
 
 <?php $this->view('module/includes/navbar') ?>
 
-<?php $data['flash']->display() ?>
+<section class="container">
+    <?php $data['flash']->display() ?>
 
-
-<!-- Ajouter -->
-<div class="menu menu-add">
-    <form method="post">
+    <h3 class="title">Gérer les promotions</h3>
     
-        <!-- Quand on clic la dessus, ca affiche le formulaire -->
-        <a href="#" class="menu-title" onclick="$(this).parent().find('.menu-hidden').toggle();">
-            Ajouter une promotion
-        </a>
+    <div class="course-list">
+        <!-- Ajouter -->
+        <div class="dashbox nopadding menu menu-add">
+            <a href="#" class="item missing menu-title" onclick="$('#add').toggle();">
+                Ajouter une promotion
+            </a>
+        </div><br>
 
-        <div class="menu-hidden" style="<?= (isset($data['method-add']) and $data['method-add']) ? '' : 'display: none;' ?>">
+        <!-- Modifier -->
+        <div class="dashbox nopadding menu menu-edit">
+            <form method="post">
+                
+                <div class="item parent menu-title">
+                    Modifier une promotion
+                </div>
 
+                <div class="item menu-select">
+                    <select name="edit" style="width: 100%">
+                        <option value="0">Sélectionnez une promotion</option>
+                        <?php foreach ($data['promotions'] as $promotion): ?>
+                            <option value="<?= $promotion->getId() ?>" <?= (isset($data['values']) and $data['values']['promotion']->getId() === $promotion->getId()) ? 'selected' : '' ?>>P<?= $promotion->getYear() ?></option>
+                        <?php endforeach; ?>
+                    </select>
+                </div>
+
+                <div class="item" style="margin-top: -20px; height: 80px">
+                    <center>
+                        <button type="submit" class="absence-submit btn-submit" style="margin-top: 0" name="method" value="retrieve">Modifier</button>
+                    </center>
+                </div>
+            </form>
+        </div><br>
+
+        <div class="dashbox nopadding menu menu-delete">
+            <form method="post">
+                <div class="item parent menu-title">
+                    Supprimer une promotion
+                </div>
+
+                <div class="item menu-select">
+                    <select name="delete" style="width: 100%">
+                        <option value="0">Sélectionnez une promotion</option>
+                        <?php foreach ($data['promotions'] as $promotion): ?>
+                            <option value="<?= $promotion->getId() ?>">P<?= $promotion->getYear() ?></option>
+                        <?php endforeach; ?>
+                    </select>
+                </div>
+
+                <div class="item" style="margin-top: -20px; height: 80px">
+                    <center>
+                        <button type="submit" class="absence-submit btn-submit" style="margin-top: 0" name="method" value="delete">Valider</button>
+                    </center>
+                </div>
+            </form>
+        </div><br>
+    </div>
+
+    <div class="dashbox calendar">
+        <form method="post" id="add" class="menu-hidden" style="<?= (isset($data['method-add']) and $data['method-add']) ? '' : 'display: none;' ?>">
+            <h3>Ajouter une promotion</h3>
+
+            <label>Année de promotion</label><br>
             <input type="number" name="year" class="menu-input" placeholder="Année (Ex: Pour P2019 entrez 2019)" value="<?= date('Y') ?>">
+            <br><br>
 
-            <button type="submit" class="btn-submit" name="method" value="add">Valider</button>
-        </div>
-
-    </form>
-</div>
-
-
-<!-- Modifier -->
-<div class="menu menu-edit">
-    <form method="post">
+            <center>
+                <button type="submit" class="absence-submit btn-submit" name="method" value="add">Valider</button>
+            </center>
+        </form>
         
-        <div class="menu-title">
-            Modifier une promotion
-        </div>
+        <?php if (isset($data['values'])): ?>
+        <form method="post" class="menu-hidden">  
+            <h3>Modifier une promotion</h3>
 
-        <!-- On choisis celui a modifier -->
-        <div class="menu-select">
-            <select name="edit">
+            <select name="edit" style="width: 100%; display: none">
                 <option value="0">Sélectionnez une promotion</option>
                 <?php foreach ($data['promotions'] as $promotion): ?>
                     <option value="<?= $promotion->getId() ?>" <?= (isset($data['values']) and $data['values']['promotion']->getId() === $promotion->getId()) ? 'selected' : '' ?>>P<?= $promotion->getYear() ?></option>
                 <?php endforeach; ?>
             </select>
-        </div>
 
-        <!-- 1. On clique sur modifier -->
-        <button type="submit" class="btn-submit" name="method" value="retrieve">Modifier</button>
+            <label>Année de promotion</label><br>
+            <input type="number" name="year" class="menu-input" placeholder="Année (Ex: Pour P2019 entrez 2019)" value="<?= $data['values']['promotion']->getYear() ?>">
 
-        <?php if (isset($data['values'])): ?>
-
-            <!-- 2. Ca charge les données dans le formulaire -->
-            <div class="menu-hidden">
-
-                <input type="number" name="year" class="menu-input" placeholder="Année (Ex: Pour P2019 entrez 2019)" value="<?= $data['values']['promotion']->getYear() ?>">
-
-                <input type="hidden" name="update" value="<?= $data['values']['promotion']->getId() ?>">
-
-                <button type="submit" class="btn-submit" name="method" value="update">Valider</button>
-            </div>
-
+            <input type="hidden" name="update" value="<?= $data['values']['promotion']->getId() ?>">
+            <center>
+                <button type="submit" class="absence-submit btn-submit" name="method" value="add">Valider</button>
+            </center>
+        </form>
         <?php endif; ?>
-
-    </form>
-</div>
-
-
-
-<div class="menu menu-delete">
-    <form method="post">
-    
-        <div class="menu-title">
-            Supprimer une promotion
-        </div>
-
-        <!-- On choisis celui a supprimer -->
-        <div class="menu-select">
-            <select name="delete">
-                <option value="0">Sélectionnez une promotion</option>
-                <?php foreach ($data['promotions'] as $promotion): ?>
-                    <option value="<?= $promotion->getId() ?>">P<?= $promotion->getYear() ?></option>
-                <?php endforeach; ?>
-            </select>
-        </div>
-
-        <button type="submit" class="btn-submit" name="method" value="delete">Valider</button>
-
-    </form>
-</div>
-
+    </div>
+</section>
